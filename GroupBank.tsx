76a58938b31ab14e46fb05b700525d2f0e474d3f
wxingheng <wxingheng@outlook.com>
2024-12-11 18:15:03 +0800

    Remove GroupBank component and its associated template from the project

--- GroupBank.tsx
+++
@@ -1,9 +0,0 @@
-import React, { memo } from 'react';
-
-interface IGroupBankProps {}
-
-const GroupBank: React.FC<IGroupBankProps> = () => {  
-  return <div className='GroupBank_component'>GroupBank</div>;
-};
-
-export default memo(GroupBank);
