b316d24dca098971c377242b3856a1c047fecb3c
wxingheng <wxingheng@outlook.com>
2024-12-11 16:00:11 +0800

    Add GroupBank component and update component generation logic

---
+++ GroupBank.component.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react';
+
+interface IGroupBankProps {}
+
+const GroupBank: React.FC<IGroupBankProps> = () => {  
+  return <div className='GroupBank_component'>GroupBank</div>;
+};
+
+export default memo(GroupBank);
